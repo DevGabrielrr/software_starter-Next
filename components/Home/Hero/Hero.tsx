c17c554817ed71aa5ea935c4a036f49bb33b6a94
Gabriel Ribeiro <bieelrr4@gmail.com>
2025-06-10 18:15:53 -0300

    feat: finish hero

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const Hero = () => {
@@ -14,9 +15,24 @@ const Hero = () => {
           <h1 className="text-4xl md:text-5xl lg:text-7xl mt-6 mb-6 font-bold text-white leading-[2.5rem] md:leading-[4rem] ">
             Providing software solutions for yuor any{" "} <span className="text-yellow-300"> business</span>
           </h1> 
+          {/* description */}
+          <p className="text-sm sm:text-base font-medium md:text-lg text-gray-300">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor odio error provident molestiae ducimus obcaecati? Id deleniti cupiditate sed.</p>
+          {/* Button */}
+          <a href="#" className="relative inline-flex items-center justify-start mt-3 px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group ">
+            <span className="w-48 h-48 rounded rotate-[-40deg] bg-purple-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+            <span className="relative w-full text-left font-bold text-black transition-colors duration-300 ease-in-out group-hover:text-white">Discover More</span>
+          </a>
         </div>
         {/* Image Content */}
-        <div></div>
+        <div className="mx-auto hidden xl:block">
+          <Image
+          src="/images/hero.png"
+          alt="Hero Image"
+          width={900}
+          height={900}
+          />
+
+        </div>
       </div>
     </div>
   );
